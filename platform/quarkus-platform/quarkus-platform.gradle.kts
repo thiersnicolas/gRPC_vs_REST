32cb0abd39db45de6a95dc436226a204a249cf75
Maarten Vandeperre @ Red Hat <mvandepe@redhat.com>
2023-05-05 18:11:55 +0200

    remove camel bom

--- platform/quarkus-platform/quarkus-platform.gradle.kts
+++ platform/quarkus-platform/quarkus-platform.gradle.kts
@@ -7,5 +7,4 @@ javaPlatform {
 }
 dependencies {
   api(platform("io.quarkus:quarkus-bom:${properties.get("quarkusVersion")}"))
-//  api(platform("org.apache.camel.quarkus:camel-quarkus-bom:3.0.0-M1"))
 }
