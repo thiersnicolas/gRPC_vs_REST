d5cdc0e47da171dec7348af4daa6d1ed4b6fa32e
Nicolas Thiers <nicolas.thiers@partners.basf.com>
2023-05-24 20:59:31 +0200

    compression

--- application_client/infrastructure/dataproviders/dataproviders-rest/src/test/java/be/hogent/nthiers/People5RestServiceTest.java
+++ application_client/infrastructure/dataproviders/dataproviders-rest/src/test/java/be/hogent/nthiers/People5RestServiceTest.java
@@ -1,14 +1,19 @@
 package be.hogent.nthiers;
 
 import be.hogent.thiersn.TestHelper;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 @QuarkusTest
 public class People5RestServiceTest {
@@ -24,82 +29,84 @@ public class People5RestServiceTest {
 
         //var amounts = List.of(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288);
 
-        var amounts = List.of(1024, 2048, 4096, 8192,9216, 10240, 11264, 12288, 13312, 14336, 15360,
+        var amounts = List.of(1024, 2048, 4096, 8192, 9216, 10240, 11264, 12288, 13312, 14336, 15360,
                 16384, 20480, 24576, 28672, 32768, 36864, 40960, 45056, 49152, 53248, 57344, 61440, 65536, 131072,
                 262144, 524288);
 
-        testHelper.testPerformance_singularRequests("test_REST_SingularCalls", amounts);
+        testHelper.testPerformance_singularRequests("test_REST_SingularCalls_24-05-2023_1", amounts);
     }
 
     @Test
     public void test_REST_compressed_SingularCalls() throws IOException {
-        var testHelper = new TestHelper<>(() -> people5RestService.clearData(), objectMapper, people5RestService::people5Compressed);
+        var testHelper = new TestHelper<>(() -> people5RestService.clearData(), objectMapper, amount ->
+                gzipDecompressBytes(people5RestService.people5Compressed(amount)));
 
         //var amounts = List.of(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288);
 
-        var amounts = List.of(1024, 2048, 4096, 8192,9216, 10240, 11264, 12288, 13312, 14336, 15360,
+        var amounts = List.of(1024, 2048, 4096, 8192, 9216, 10240, 11264, 12288, 13312, 14336, 15360,
                 16384, 20480, 24576, 28672, 32768, 36864, 40960, 45056, 49152, 53248, 57344, 61440, 65536, 131072,
                 262144, 524288);
 
-        testHelper.testPerformance_singularRequests("test_REST_compressed_SingularCalls", amounts);
+        testHelper.testPerformance_singularRequests("test_REST_compressed_SingularCalls_24-05-2023_1", amounts);
     }
+
     @Test
     public void test_REST_MultipleBatchedCalls_DifferentBatchSizes() throws IOException {
         var testHelper = new TestHelper<>(() -> people5RestService.clearData(), objectMapper, people5RestService::people5);
 
         var amounts = List.of(4194304);
+/**
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize1024", amounts, 1024);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize1024", amounts, 1024);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize1024 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize1024 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize2048", amounts, 2048);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize2048", amounts, 2048);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize2048 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize2048 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize4096", amounts, 4096);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize4096", amounts, 4096);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize4096 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize4096 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize8192", amounts, 8192);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize8192", amounts, 8192);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize8192 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize8192 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize9216", amounts, 9216);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize9216", amounts, 9216);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize9216 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize9216 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize10240", amounts, 10240);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize10240", amounts, 10240);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize10240 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize10240 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize11264", amounts, 11264);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize11264", amounts, 11264);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize11264 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize11264 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize12288", amounts, 12288);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize12288", amounts, 12288);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize12288 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize12288 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize13312", amounts, 13312);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize13312", amounts, 13312);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize13312 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize13312 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize14336", amounts, 14336);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize14336", amounts, 14336);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize14336 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize14336 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize15360", amounts, 15360);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize15360", amounts, 15360);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize15360 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize15360 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize16384", amounts, 16384);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize16384", amounts, 16384);
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize16384 done");
 
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize16384 done");
+ testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize20480", amounts, 20480);
 
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize20480", amounts, 20480);
-
-        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize20480 done");
+ System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize20480 done");**/
 
         testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize24576", amounts, 24576);
 
@@ -144,6 +151,18 @@ public class People5RestServiceTest {
         testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize65536", amounts, 65536);
 
         System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize65536 done");
+
+        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize131072", amounts, 131072);
+
+        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize131072 done");
+
+        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize262144", amounts, 262144);
+
+        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize262144 done");
+
+        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize524288", amounts, 524288);
+
+        System.out.println("test_REST_MultipleBatchedCalls_DifferentBatchSizes_batchSize524288 done");
     }
 
     @Test
@@ -247,6 +266,18 @@ public class People5RestServiceTest {
         testHelper.testPerformance_multipleRequests("test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_batchSize65536", amounts, 65536);
 
         System.out.println("test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_batchSize65536 done");
+
+        testHelper.testPerformance_multipleRequests("test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_batchSize131072", amounts, 131072);
+
+        System.out.println("test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_batchSize131072 done");
+
+        testHelper.testPerformance_multipleRequests("test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_batchSize262144", amounts, 262144);
+
+        System.out.println("test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_batchSize262144 done");
+
+        testHelper.testPerformance_multipleRequests("test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_batchSize524288", amounts, 524288);
+
+        System.out.println("test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_batchSize524288 done");
     }
 
     @Test
@@ -261,34 +292,54 @@ public class People5RestServiceTest {
     }
 
     @Test
-    public void test_REST_gzip_MultipleBatchedCalls_DifferentBatchSizes_justOne() throws IOException {
-        var testHelper = new TestHelper<>(() -> people5RestService.clearData(), objectMapper, people5RestService::people5);
+    public void test_REST_compressed_MultipleBatchedCalls_DifferentBatchSizes_justOne() throws IOException {
+        var testHelper = new TestHelper<>(() -> people5RestService.clearData(), objectMapper, people5RestService::people5Compressed);
 
         var amounts = List.of(4194304);
 
-        testHelper.testPerformance_multipleRequests("test_REST_gzip_MultipleBatchedCalls_DifferentBatchSizes_batchSize524288", amounts, 524288);
+        testHelper.testPerformance_multipleRequests("test_REST_gzip_MultipleBatchedCalls_DifferentBatchSizes_batchSize65536", amounts, 65536);
 
-        System.out.println("test_REST_gzip_MultipleBatchedCalls_DifferentBatchSizes_batchSize524288 done");
+        System.out.println("test_REST_gzip_MultipleBatchedCalls_batchSize655368 done");
     }
 
     @Test
     public void test_REST_MultipleBatchedCalls() throws IOException {
         var testHelper = new TestHelper<>(() -> people5RestService.clearData(), objectMapper, people5RestService::people5);
 
-        var amounts = List.of(1024, 2048, 4096, 8192,9216, 10240, 11264, 12288, 13312, 14336, 15360,
+        var amounts = List.of(1024, 2048, 4096, 8192, 9216, 10240, 11264, 12288, 13312, 14336, 15360,
                 16384, 20480, 24576, 28672, 32768, 36864, 40960, 45056, 49152, 53248, 57344, 61440, 65536, 131072,
                 262144, 524288, 1048576, 1572864, 2097152, 2621440, 3145728, 3670016, 4194304);
-        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_BatchSize_12288", amounts, 12288);
+        testHelper.testPerformance_multipleRequests("test_REST_MultipleBatchedCalls_BatchSize_65536", amounts, 65536);
     }
 
     @Test
     public void test_REST_gzip_MultipleBatchedCalls() throws IOException {
-        var testHelper = new TestHelper<>(() -> people5RestService.clearData(), objectMapper, people5RestService::people5);
+        var testHelper = new TestHelper<>(() -> people5RestService.clearData(), objectMapper, people5RestService::people5Compressed);
 
-        var amounts = List.of(1024, 2048, 4096, 8192,9216, 10240, 11264, 12288, 13312, 14336, 15360,
+        var amounts = List.of(1024, 2048, 4096, 8192, 9216, 10240, 11264, 12288, 13312, 14336, 15360,
                 16384, 20480, 24576, 28672, 32768, 36864, 40960, 45056, 49152, 53248, 57344, 61440, 65536, 131072,
                 262144, 524288, 1048576, 1572864, 2097152, 2621440, 3145728, 3670016, 4194304);
         testHelper.testPerformance_multipleRequests("test_REST_gzip_MultipleBatchedCalls_BatchSize_12288", amounts, 12288);
+    }
+
+    public <T> List<T> gzipDecompressBytes(byte[] compressed) {
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            final int BUFFER_SIZE = 32;
+            ByteArrayInputStream is = new ByteArrayInputStream(compressed);
+            GZIPInputStream gis = new GZIPInputStream(is, BUFFER_SIZE);
+            byte[] data = new byte[BUFFER_SIZE];
+            int bytesRead;
+            while ((bytesRead = gis.read(data)) != -1) {
+                os.write(data, 0, bytesRead);
+            }
+            gis.close();
+            return objectMapper.readValue(os.toByteArray(), new TypeReference<>() {
+            });
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
 }
--- application_server/infrastructure/entrypoints/entrypoints-rest/src/main/java/be/hogent/nthiers/People5Resource.java
+++ application_server/infrastructure/entrypoints/entrypoints-rest/src/main/java/be/hogent/nthiers/People5Resource.java
@@ -1,24 +1,26 @@
 package be.hogent.nthiers;
 
-import io.quarkus.vertx.http.Compressed;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.vertx.http.Uncompressed;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Encoded;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.jboss.resteasy.annotations.GZIP;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Collection;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 
 @Path("/people5")
 public class People5Resource {
     @Inject
     PeopleInMemoryRepository peopleInMemoryRepository;
+    @Inject
+    ObjectMapper objectMapper;
 
     @Path("/cleardata")
     @POST
@@ -38,8 +40,25 @@ public class People5Resource {
 
     @Path("/{amount}/compressed")
     @GET
-    @Compressed
-    public List<Person5> people5compressed(@PathParam("amount") Integer amount) {
-        return peopleInMemoryRepository.getPeople5(amount);
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public byte[] people5compressed(@PathParam("amount") Integer amount) {
+        return gzip(peopleInMemoryRepository.getPeople5(amount));
+    }
+
+    public <T> byte[] gzip(final List<T> o) {
+        try {
+            if (o == null) {
+                return null;
+            }
+            try (final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                 final GZIPOutputStream gzip = new GZIPOutputStream(baos)) {
+                gzip.write(objectMapper.writeValueAsBytes(o));
+                gzip.close();
+                return baos.toByteArray();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
